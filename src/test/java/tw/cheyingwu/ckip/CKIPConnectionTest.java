5b1f26da62f85f168e7b84105d2752a75c3f6667
onlinemad-desktop-ubuntu <onlinemad@gmail.com>
2013-03-20 22:57:51 +0800

    Add a properties to save ckip server information. close #12

--- src/test/java/tw/cheyingwu/ckip/CKIPConnectionTest.java
+++ src/test/java/tw/cheyingwu/ckip/CKIPConnectionTest.java
@@ -2,6 +2,12 @@ package tw.cheyingwu.ckip;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,10 +15,10 @@ import tw.cheyingwu.ckip.util.CKIPMessageHelper;
 
 public class CKIPConnectionTest {
 	
-	private String username = "username";
-	private String password = "password";
-	private String ip = "ip";
-	private int port = 9999;
+	private String username;
+	private String password;
+	private String ip;
+	private Integer port;
 	
 	private String expectXML1 = "<?xml version=\"1.0\" ?><wordsegmentation version=\"0.1\"><processstatus code=\"0\">Success</processstatus><result><sentence>　台新(N)　金控(N)　12月(N)　3日(N)　將(ADV)　召開(Vt)　股東(N)　臨時會(N)　進行(Vt)　董監(N)　改選(Nv)　。(PERIODCATEGORY)</sentence></result></wordsegmentation>";
 	private String expectXML2 = "<?xml version=\"1.0\" ?><wordsegmentation version=\"0.1\"><processstatus code=\"0\">Success</processstatus><result><sentence>　中華隊(N)　的(T)　未來(N)　還是(ADV)　充滿(Vt)　了(ASP)　希望(Vt)　。(PERIODCATEGORY)</sentence></result></wordsegmentation>";
@@ -21,7 +27,21 @@ public class CKIPConnectionTest {
 	
 	@Before
 	public void init(){
-		ckipConn = new CKIPConnection(ip, port);
+		try {
+			Properties p = new Properties();
+			p.load(new FileReader(new File("properties/ckip.properties")));
+			
+			username = p.getProperty("ckipConnection.username");
+			password = p.getProperty("ckipConnection.password");
+			ip = p.getProperty("ckipConnection.ip");
+			port = Integer.parseInt(p.getProperty("ckipConnection.port"));
+			
+			ckipConn = new CKIPConnection(ip, port);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Test
